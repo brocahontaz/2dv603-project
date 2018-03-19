7e8dc8901be02636098fde306effcc50df8979a3
Johan Andersson <900304@gmail.com>
2018-03-19 17:59:22 +0100

    DBParser work
    More work done on the database integration.

--- src/controller/DBParser.java
+++ src/controller/DBParser.java
@@ -6,25 +6,44 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DBParser {
-	
+
+	private Connection connection;
+	private String url = "";
+	private String user = "";
+	private String password = "";
+
 	private PreparedStatement ps = null;
-	
+
 	public DBParser() {
-		
+
 	}
-	
+
 	private void executeQuery(String query, String[] params) {
-		
+
 	}
-	
+
 	private void initialize() {
-		
+		try {
+			connection = DriverManager.getConnection(url, user, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	private void shutdown() {
-		
+		try {
+			connection.setAutoCommit(true);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 }
-
-
