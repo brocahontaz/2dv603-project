8e69dab21dad165b8b84ab142711fe31a4621184
Johan Andersson <900304@gmail.com>
2018-03-19 17:36:38 +0100

    Database integration started
    Started work on a database parser class.

---
+++ src/controller/DBParser.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DBParser {
+	
+	private PreparedStatement ps = null;
+	
+	public DBParser() {
+		
+	}
+	
+	private void executeQuery(String query, String[] params) {
+		
+	}
+	
+	private void initialize() {
+		
+	}
+	
+	private void shutdown() {
+		
+	}
+
+}
+
+
