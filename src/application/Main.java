505793f1ad75de1b6290f97a43dbf9ed8c7cee30
Johan Andersson <900304@gmail.com>
2018-03-26 19:14:17 +0200

    Relocated view files
    Moved view files into proper package

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,9 +11,9 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("ViewConfig.fxml"));
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("/view/ViewConfig.fxml"));
 			Scene scene = new Scene(root,800,600);
-			scene.getStylesheets().add(getClass().getResource("bootstrap3.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("/view/bootstrap3.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setMaximized(true);
 			primaryStage.setMinHeight(600);
