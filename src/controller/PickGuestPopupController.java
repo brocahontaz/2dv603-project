3ad5689fde6a2dd3088f89a10318e92e2f0223fd
David Larsson <manzey@hotmail.se>
2018-04-04 03:19:54 +0200

    Fill table upon initialization & click event for rows added
    Click events for rows now prints the row

--- src/controller/PickGuestPopupController.java
+++ src/controller/PickGuestPopupController.java
@@ -1,30 +1,84 @@
 package controller;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import model.Guest;
 
 public class PickGuestPopupController {
+
+	private ObservableList<Guest> guests;
+	private ExecutorService executor = Executors.newSingleThreadExecutor();
+	private DBParser dbParser = new DBParser();
+
+	@FXML
+	private Button popupGuestSearchButton;
+
+	@FXML
+	private Button closeGuestsPopUpButton;
+
+	@FXML
+	private TableView<model.Guest> guestsResultTable;
+
+	@FXML
+	private TextField popupGuestSearch;
+
+	@FXML
+	private TableColumn<model.Guest, String> popFirstNameCol;
+
+	@FXML
+	private TableColumn<model.Guest, String> popLastNameCol;
+
+	@FXML
+	private TableColumn<model.Guest, String> popPassportCol;
+
+	@FXML
+	private TableColumn<model.Guest, String> popTelephoneCol;
+
+	@FXML
+	void closeGuestsPopUp(MouseEvent event) {
+		((Node) (event.getSource())).getScene().getWindow().hide();
+	}
+
+	@FXML
+	void popupGuestSearch(MouseEvent event) {
+	}
 	
+	/**
+	 * Get the Guest when double clicking on row.
+	 */
 	@FXML
-    private Button popupGuestSearchButton;
-
-    @FXML
-    private Button closeGuestsPopUpButton;
-    
-    @FXML
-    private TextField popupGuestSearch;
-    
+	private void getGuestData(MouseEvent event) {
+		if (event.getClickCount() == 2) {
+			Guest guest = guestsResultTable.getSelectionModel().getSelectedItem();
+			System.out.println(guest);		
+		}
+	}
+	
+	/**
+	 * Load Guest(s) upon opening of the window.
+	 */
 	@FXML
-    void closeGuestsPopUp(MouseEvent event) {
-		((Node)(event.getSource())).getScene().getWindow().hide();
-    }
+	void initialize() {
+		popFirstNameCol.setCellValueFactory(new PropertyValueFactory<model.Guest, String>("firstName"));
+		popLastNameCol.setCellValueFactory(new PropertyValueFactory<model.Guest, String>("lastName"));
+		popPassportCol.setCellValueFactory(new PropertyValueFactory<model.Guest, String>("passportNumber"));
+		popTelephoneCol.setCellValueFactory(new PropertyValueFactory<model.Guest, String>("telephoneNumber"));
+		
+		executor.submit(() -> {
+			guests = FXCollections.observableArrayList(dbParser.getAllGuests());
+			guestsResultTable.setItems(guests);
 
-    @FXML
-    void popupGuestSearch(MouseEvent event) {
-
-    }
-
+		});
+	}
 }
