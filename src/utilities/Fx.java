153c9e4ca006291a2e1ffbf3aaaf32a529836153
Fredrik Norrman <fredden87@gmail.com>
2018-04-19 18:32:14 +0200

    completely remade titledPaneColorNotification whitout use of threads
    completly remade titledPaneColorNotification whitout use of threads, this will hopefully work alot better!

--- src/utilities/Fx.java
+++ src/utilities/Fx.java
@@ -1,19 +1,18 @@
 package utilities;
 
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import javafx.animation.PauseTransition;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
-
-
+import javafx.util.Duration;
 
 public class Fx {
-	
-	private static ExecutorService executor = Executors.newFixedThreadPool(1);
-	
+
 	/**
 	 * Clear 6 textfields.
+	 * 
 	 * @param textField1
 	 * @param textField2
 	 * @param textField3
@@ -21,7 +20,8 @@ public class Fx {
 	 * @param textField5
 	 * @param textField6
 	 */
-	public static void textFieldClear(TextField textField1, TextField textField2, TextField textField3, TextField textField4,TextField textField5 ,TextField textField6) {
+	public static void textFieldClear(TextField textField1, TextField textField2, TextField textField3,
+			TextField textField4, TextField textField5, TextField textField6) {
 		textField1.clear();
 		textField2.clear();
 		textField3.clear();
@@ -29,9 +29,10 @@ public class Fx {
 		textField5.clear();
 		textField6.clear();
 	}
-	
+
 	/**
 	 * Clear 8 textfields.
+	 * 
 	 * @param textField1
 	 * @param textField2
 	 * @param textField3
@@ -41,7 +42,9 @@ public class Fx {
 	 * @param textField7
 	 * @param textField8
 	 */
-	public static void textFieldClear(TextField textField1, TextField textField2, TextField textField3, TextField textField4,TextField textField5 ,TextField textField6, TextField textField7, TextField textField8) {
+	public static void textFieldClear(TextField textField1, TextField textField2, TextField textField3,
+			TextField textField4, TextField textField5, TextField textField6, TextField textField7,
+			TextField textField8) {
 		textField1.clear();
 		textField2.clear();
 		textField3.clear();
@@ -51,9 +54,10 @@ public class Fx {
 		textField7.clear();
 		textField8.clear();
 	}
-	
+
 	/**
 	 * Changes the css-style of a textfield for 3 seconds.
+	 * 
 	 * @param textField
 	 * @param cssStyle
 	 */
@@ -75,56 +79,47 @@ public class Fx {
 		});
 		executor.shutdown();
 	}
-	
+
 	/**
 	 * Changes the css-style of a titledpane for 3 seconds.
+	 * 
 	 * @param titledPane
 	 * @param cssStyle
 	 */
 	public static void titledPaneColorNotification(TitledPane titledPane, String cssStyle) {
-		executor.submit(() -> {
-			try {
-				String old = titledPane.getStyleClass().toString();
-				String oldStyle = trimCssStyle(old);
-				titledPane.getStyleClass().remove(oldStyle);
-				titledPane.getStyleClass().add(cssStyle);
-				Thread.sleep(3000);
-				titledPane.getStyleClass().remove(cssStyle);
-				titledPane.getStyleClass().add(oldStyle);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-				System.out.println("Exception from FX titledPaneColorNotification 3sec");
-			}
-		});
+		String oldStyle = trimCssStyle(titledPane.getStyleClass().toString());
+		titledPane.getStyleClass().remove(oldStyle);
+		titledPane.getStyleClass().add(cssStyle);
+		PauseTransition pause = new PauseTransition(Duration.seconds(3));
+		pause.setOnFinished(event -> titledPaneColorNotificationHelp(titledPane, cssStyle, oldStyle));
+		pause.play();
 	}
-	
+
+	private static void titledPaneColorNotificationHelp(TitledPane titledPane, String cssStyle, String oldStyle) {
+		titledPane.getStyleClass().remove(cssStyle);
+		titledPane.getStyleClass().add(oldStyle);
+	}
+
 	/**
-	 * Changes the css-style of a titlepane for x milliseconds.
+	 * Changes the css-style of a titlepane for x seconds.
+	 * 
 	 * @param titledPane
 	 * @param cssStyle
 	 * @param time
 	 */
 	public static void titledPaneColorNotification(TitledPane titledPane, String cssStyle, int time) {
-		executor.submit(() -> {
-			try {
-				String old = titledPane.getStyleClass().toString();
-				String oldStyle = trimCssStyle(old);
-				titledPane.getStyleClass().remove(oldStyle);
-				titledPane.getStyleClass().add(cssStyle);
-				Thread.sleep(time);
-				titledPane.getStyleClass().remove(cssStyle);
-				titledPane.getStyleClass().add(oldStyle);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-				System.out.println("Exception from FX titledPaneColorNotification x ms");
-			}
-		});
+		String oldStyle = trimCssStyle(titledPane.getStyleClass().toString());
+		titledPane.getStyleClass().remove(oldStyle);
+		titledPane.getStyleClass().add(cssStyle);
+		PauseTransition pause = new PauseTransition(Duration.seconds(time));
+		pause.setOnFinished(event -> titledPaneColorNotificationHelp(titledPane, cssStyle, oldStyle));
+		pause.play();
 	}
-	
+
 	// Helper method to trim cssString.
 	private static String trimCssStyle(String cssStyle) {
 		String result = "";
-		result = cssStyle.substring(cssStyle.lastIndexOf(" ")+1);
+		result = cssStyle.substring(cssStyle.lastIndexOf(" ") + 1);
 		return result;
 	}
 
